95b97642fb5fcd63ff785b8a647623d41a92db01
zy <3328039835@qq.com>
2025-06-17 12:40:25 +0800

    Btb.v0.0

--- playground/src/BTB/copy_BTB.scala
+++ playground/src/BTB/copy_BTB.scala
@@ -103,7 +103,7 @@ class My_Btb extends Module with BP_Utail{
         val out_0 = Output(new  Btb_data)
         val out_1 = Output(new  Btb_data)
 
-        val updata = Input(new Btb_update_entry)
+        val update = Input(new Btb_update_entry)
     })
 //-----------------------读取指令的历史记录，明白跳转的目标-------------------------------//
 
@@ -113,12 +113,10 @@ class My_Btb extends Module with BP_Utail{
 
     val bank_idx_0 = Wire(UInt(log2Ceil(Btb_entrys).W))
     val bank_idx_1 = Wire(UInt(log2Ceil(Btb_entrys).W))
-    val bank_idx_u = Wire(UInt(log2Ceil(Btb_entrys).W))
 
     val tag_0 = Wire(UInt(tagsize.W))
     val tag_1 = Wire(UInt(tagsize.W))
-    val tag_u = Wire(UInt(tagsize.W))
-    
+
     bank_idx_0 := get_idx(io.in_0.req_pc,32,8)
     bank_idx_1 := get_idx(io.in_1.req_pc,32,8)
 
@@ -158,16 +156,20 @@ class My_Btb extends Module with BP_Utail{
     io.out_1.valid := hit_1
 
 //-------------------------------更新策略-----------------------------//
-    val rEntry_u =  Wire(Vec(Btb_ways,new Btb_entry)) 
-    rEntry_u := Btb_bank(bank_idx_u) 
-    bank_idx_u := get_idx(io.updata.update_pc,32,idx_len)
-    tag_u := seg(io.updata.update_pc,2,tagsize)
+    val rEntry_u =  Wire(Vec(Btb_ways,new Btb_entry))     
+    val bank_idx_u = Wire(UInt(log2Ceil(Btb_entrys).W))
+    val tag_u = Wire(UInt(tagsize.W))
     val hits_u =  (0 until Btb_ways).map(i => rEntry_u(i).tag === tag_u && rEntry_u(i).have)
-    val lfsr = LFSR(log2Ceil(Btb_sets), io.updata.require)
+    val lfsr = LFSR(log2Ceil(Btb_sets), io.update.require)
 
-    val w_way = UInt(1.W)
+    bank_idx_u := get_idx(io.update.update_pc,32,idx_len)
+    rEntry_u := Btb_bank(bank_idx_u) 
+    tag_u := seg(io.update.update_pc,2,tagsize)
+
 /*     val update_require_r = Reg(Bool())
-    update_require_r := io.updata.require  //todo 大概需要完善 */
+    update_require_r := io.update.require  //大概需要完善 */
+    //*选择路的策略：1.命中时更新数据  2.哪一个为空写入哪一个 3.随机输入一个
+    val w_way = UInt(1.W)
     when(hits_u.reduce(_ || _) ){
         w_way := Mux(hits_u(0),0.U,1.U)
     }.elsewhen(!rEntry_u(0).have){
@@ -178,17 +180,33 @@ class My_Btb extends Module with BP_Utail{
         w_way := (lfsr ^ bank_idx_u).xorR  //
     }
 
-/*     when(io.updata.require){
-        Btb_bank(bank_idx_u)(w_way).data := 
+    //有更新需求时直接更新，同时更新have位
+    when(io.update.require){
+        Btb_bank(bank_idx_u)(w_way).data := io.update.data
     }
- */
 
+    when(io.update.require && !Btb_bank(bank_idx_u)(w_way).have){
+        Btb_bank(bank_idx_u)(w_way).have := !Btb_bank(bank_idx_u)(w_way).have
+    }
 
+//-----------------------------加入预测器-------------------------------//
+    val predictor = Module(new my_Predictor)
+    predictor.io.pc_in0 := io.in_0
+    predictor.io.pc_in1 := io.in_1
+    //读取数据
+    io.out_0.brTaken := predictor.io.Taken_0
+    io.out_1.brTaken := predictor.io.Taken_1
+    //更新的接口
+    predictor.io.update.valid := io.update.require
+    predictor.io.update.brTaken := io.update.data.brTaken
+    predictor.io.update.Pre_pc := io.update.update_pc
 
 } 
 
+
+
 //BHT和PHT进行预测是否该跳转
-//---------------------------------预测器------------------------------//
+//-------------------------------预测器定义-------------------------------//
 trait  Predictor_Queue {
     val nPHT = 128
     val PHTLEN = log2Ceil(nPHT)
@@ -198,7 +216,7 @@ trait  Predictor_Queue {
 }    
 
 class Pre_Entry extends Bundle {
-    val valid = Bool()
+    val valid = Bool() //更新需求位
     val brTaken = Bool()
     val Pre_pc = UInt(32.W)
 }
@@ -230,10 +248,9 @@ class my_Predictor extends Module with Predictor_Queue with BP_Utail{
 //----------------------------读取操作---------------------------//    
     val bhtBank_idx_0 = Wire(UInt(BHTLEN.W))
     val bhtBank_idx_1 = Wire(UInt(BHTLEN.W))
-    val bhtBank_idx_u = Wire(UInt(BHTLEN.W))
     val phtBank_idx_0 = Wire(UInt(PHTLEN.W))
     val phtBank_idx_1 = Wire(UInt(PHTLEN.W))
-    val phtBank_idx_u = Wire(UInt(PHTLEN.W))
+
     
     bhtBank_idx_0 := get_idx(io.pc_in0,32,BHTLEN)
     bhtBank_idx_1 := get_idx(io.pc_in1,32,BHTLEN)
@@ -248,11 +265,32 @@ class my_Predictor extends Module with Predictor_Queue with BP_Utail{
     io.Taken_1 := phtBank(phtBank_idx_1).ctr(1) && phtBank(phtBank_idx_1).valid
 
 //-----------------------------更新策略--------------------------//
+    val bhtBank_idx_u = Wire(UInt(BHTLEN.W))
+    val phtBank_idx_u = Wire(UInt(PHTLEN.W))
+    val bhr_u = UInt(BHRLEN.W)
+    val ctr_u = UInt(2.W)
+    val zero = UInt(1.W)
+    zero := 0.U
+    bhtBank_idx_u := get_idx(io.update.Pre_pc,32,BHTLEN)
+    bhr_u := bhtBank(bhtBank_idx_u).bhr
+    phtBank_idx_u := bhr_u ^ bhtBank_idx_u  
+    ctr_u := phtBank(phtBank_idx_u).ctr
 
-
-
-
-
+//当有更新请求时进行更新，
+//bht和pht数据更新依据是否写过，写过就正常更新，没写过就更新valid位
+    when(io.update.valid){
+        when(bhtBank(bhtBank_idx_u).valid){
+            bhtBank(bhtBank_idx_u).bhr := Cat(bhtBank(bhtBank_idx_u).bhr(BHRLEN - 2,0),io.update.brTaken)
+        }.otherwise{
+            bhtBank(bhtBank_idx_u).bhr := Cat(zero,io.update.brTaken)
+            bhtBank(bhtBank_idx_u).valid := !bhtBank(bhtBank_idx_u).valid
+        }
+        when(phtBank(phtBank_idx_u).valid){
+            phtBank(phtBank_idx_u).ctr :=BP_update(ctr_u,2,io.update.brTaken)
+        }.otherwise{
+            phtBank(phtBank_idx_u).valid := !phtBank(phtBank_idx_u).valid
+        }
+    }
 
 }
 
